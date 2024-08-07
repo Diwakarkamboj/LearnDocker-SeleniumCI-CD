06c9ca213dabf68e7379776fb2fc559f6f96d180
Diwakarkamboj <diwakarkumar096@gmail.com>
2024-08-07 21:01:07 +1000

    Implemented Parallel Execution in Grid via docker-compose and scaled number of instances chrome and firefox in yaml file

--- 03-Automation-Framework/selenium-docker/src/test/java/com/diwakar15/selenium_docker/tests/AbstractTest.java
+++ 03-Automation-Framework/selenium-docker/src/test/java/com/diwakar15/selenium_docker/tests/AbstractTest.java
@@ -1,9 +1,17 @@
 package com.diwakar15.selenium_docker.tests;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -12,11 +20,41 @@ public abstract class AbstractTest {
 	protected WebDriver driver;
 
 	@BeforeTest
-	public void setDriver() {
+//	@Parameters({"browser"}) TO CHANGE BROWSER FOR EVERY TEST
+	//public void setDriver(String browser) throws MalformedURLException {
+	public void setDriver() throws MalformedURLException {
+		if(Boolean.getBoolean("selenium.grid.enabled")) {
+			//this.driver = getRemoteDriver(browser);
+			this.driver = getRemoteDriver();
+		}else
+		{
+			this.driver = getLocalDriver();
+		}
+	}
+	
+	//	private WebDriver getRemoteDriver(String browser) throws MalformedURLException {
+	private WebDriver getRemoteDriver() throws MalformedURLException {
+		
+		Capabilities capabilities;
+		
+		if(System.getProperty("browser").equalsIgnoreCase("chrome"))
+		//if(browser.equalsIgnoreCase("chrome"))
+		{
+			capabilities = new ChromeOptions();
+		}else 
+		{
+			capabilities = new FirefoxOptions();
+		}
+		
+		return new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"),capabilities);	
+		
+	}
+	
+	
+	private WebDriver getLocalDriver() {
 		WebDriverManager.chromedriver().browserVersion("127.0.6533.73").setup();
-		this.driver = new ChromeDriver();
-		driver.manage().window().maximize();
-
+		return new ChromeDriver();	
+		
 	}
 
 	@AfterTest
